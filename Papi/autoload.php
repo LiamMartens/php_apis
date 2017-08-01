8eefb6c196e896bbdd6e917921daf9d7d33abdf2
Liam Martens <hi@liammartens.com>
2017-08-01 15:28:20 +0200

    Small comment

--- Papi/autoload.php
+++ Papi/autoload.php
@@ -1,4 +1,5 @@
 <?php
+    # for non composer installation
     spl_autoload_register(function($class) {
         if(substr($class, 0, 5)=='Papi\\') {
            $fileName = str_replace('\\', '/', __DIR__.'/'.substr($class, 5).'.php');
